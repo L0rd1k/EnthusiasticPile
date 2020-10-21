d6e85f2b54863c6e6371f1873f7e26c8b0d9017e
Ilya Petrikov <ilua_1108@mail.ru>
2020-10-21 13:23:32 +0300

    Saving scrapping info to Pandas table

--- WebScrapping/DataCleaning.py
+++ WebScrapping/DataCleaning.py
@@ -1,65 +1,92 @@
 import requests
 from bs4 import BeautifulSoup
-import pickle
 import os
-
-
-def getArticleTitle(soup):
-    title = [p.text for p in soup.find(class_="title").find_all('h1')]
-    return title
-
-
-def getArticleText(soup):
-    a_text = [p.text for p in soup.find(class_="node-article").find_all('p')]
-    return a_text
-
-
-def sitesDataToTranscript(url):
-    page = requests.get(url).text
-    soup = BeautifulSoup(page, "lxml")
-    r_title = getArticleTitle(soup)
-    r_text = getArticleText(soup)
-    dictionary = dict()
-    print(r_text[0])
-    for i, c in enumerate(r_title):
-        dictionary.update({r_title[i]:r_text[i]})
-    return dictionary
-
-
+import pandas as pd
+import re
+import string
 
 urls = ['https://learnenglish.britishcouncil.org/general-english/stories/a-serious-case',
         'https://learnenglish.britishcouncil.org/general-english/video-zone/black-british-history',
         'https://learnenglish.britishcouncil.org/general-english/video-zone/ten-cat-facts'
         ]
-articles = dict()
-articles = [sitesDataToTranscript(u) for u in urls]
 
-res = [ele for key in articles for ele in key]
+class SiteScrapper:
+    article_title = list()
+    article_text = list()
+    articles = dict()
 
-try:
-    os.mkdir("transcripts")
-except OSError:
-    print("Creation of the directory failed")
-else:
-    print("Successfully created the directory!")
+    def __init__(self, urls):
+        self.urls = urls
 
-for key in articles:
-    for ele in key:
-        with open("transcripts/article_" + str(ele) + ".txt","w+") as file:
-            file.writelines(key.get(ele))
+    def getArticleTitle(self, soup):
+        article_title = [p.text for p in soup.find(class_="title").find_all('h1')]
+        return article_title
+
+    def getArticleText(self,soup):
+        article_text = [p.text for p in soup.find(class_="node-article").find_all('p')]
+        return article_text
+
+    def sitesDataToSoup(self, url):
+        page = requests.get(url).text
+        soup = BeautifulSoup(page, "lxml")
+        r_title = self.getArticleTitle(soup)
+        r_text = self.getArticleText(soup)
+        dictionary = dict()
+        for i, c in enumerate(r_title):
+            dictionary.update({r_title[i]: r_text})
+        return dictionary
+
+    def getArticlePagesInfo(self):
+        articles = [self.sitesDataToSoup(url) for url in self.urls]
+        return articles
 
 
+class UtilsFunction():
+    def __init__(self):
+        pass
 
-# for i, c in enumerate(articles):
-#     with open("transcripts/article_" + str(articles[0].keys()) + ".txt","w+") as file:
-#         file.writelines(articles[0].get)
-#         pickle.dump(articles[i][1], file)
+    def clean_text_round1(self, text):
+        text = text.lower()
+        text = re.sub('\[.*?\]', '', text)
+        text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+        text = re.sub('\w*\d\w*', '', text)
+        return text
 
-#
-# data = {}
-# for i, c in enumerate(transcripts):
-#     with open("transcripts/article" + str(i) + ".txt", "rb") as file:
-#         data[i] = pickle.load(file)
-#
-# print(data.keys())
-# print(data[0])
+    def textCombination(self, list_of_text):
+        combined_text = ''.join(list_of_text)
+        return combined_text
+
+
+def main():
+    scrapper = SiteScrapper(urls)
+    articles = scrapper.getArticlePagesInfo()
+    utils = UtilsFunction()
+
+    try:
+        os.mkdir("transcripts")
+    except OSError:
+        pass
+    else:
+        print("Successfully created the directory!")
+    for key in articles:
+        for ele in key:
+            with open("transcripts/article_" + str(ele) + ".txt","w+") as file:
+                file.writelines(key.get(ele))
+
+    data = {}
+    for key in articles:
+        for ele in key:
+            with open("transcripts/article_" + str(ele) + ".txt","r+") as file:
+                data[ele] = key.get(ele)
+
+    data_combined = {key: [utils.textCombination(value)] for (key,value) in data.items()}
+    pd.set_option('max_colwidth', 150)
+    data_df = pd.DataFrame.from_dict(data_combined).transpose()
+    data_df.columns = ['Story']
+    data_df = data_df.sort_index()
+    print(data_df.head())
+
+
+if __name__ == "__main__":
+    main()
+
