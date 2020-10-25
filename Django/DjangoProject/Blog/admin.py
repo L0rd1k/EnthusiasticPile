1fe6fe23b10484ae3a7c4bb105e46976c631c90b
Ilya <ilua107@gmail.com>
2020-10-25 20:31:19 +0300

    Add get request for category (DRF)

--- Django/DjangoProject/Blog/admin.py
+++ Django/DjangoProject/Blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Category, Actor, Genre, Movie, MovieShots, RatingStar, Rating, Review
 # Register your models here.
+#
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "url")
+    list_display_links = ("name",)
--- Django/DjangoProject/Blog/views.py
+++ Django/DjangoProject/Blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+
+class CategoryListView(APIView):
+    def get(self,request):
+        category = Category.objects.all()
+        serializer = CategorySerializer(category, many=True)
+        return Response(serializer.data)
